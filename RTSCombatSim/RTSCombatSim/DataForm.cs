f2e2bcde3ca220d7d31247de64428c47ee32b97f
chungbung22 <cc733@cornell.edu>
2014-03-02 00:12:05 -0500

    Added labels, boxes, event handler for DataForm
    Added labels, combo and text boxes, button to add unit, and event
    handlers to DataForm.cs. Created layout for form in
    DataForm.Designers.cs

--- RTSCombatSim/RTSCombatSim/DataForm.cs
+++ RTSCombatSim/RTSCombatSim/DataForm.cs
@@ -33,6 +33,97 @@ namespace RTSCS {
             if(!e.Cancel) Instance = null;
         }
 
+        int selectedIndex;
+        int counter = 1;
+        String[] stats;
+        String[] spawn1;
+        String[] spawn2;
+        String[] spawn3;
+
+        private void DataForm_Load(object sender, EventArgs e)
+        {
+            Instance = this;
+        }
+        private void DataForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!e.Cancel) Instance = null;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedIndex = comboBox1.SelectedIndex;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Unit " + counter);
+            counter++;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            stats[0] = textBox1.Text;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            stats[1] = textBox2.Text;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            stats[2] = textBox3.Text;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            stats[3] = textBox4.Text;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            spawn1[0] = textBox5.Text;
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            spawn1[1] = textBox8.Text;
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            spawn1[2] = textBox6.Text;
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            spawn2[0] = textBox10.Text;
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            spawn2[1] = textBox9.Text;
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            spawn2[2] = textBox7.Text;
+        }
+
+        private void textBox13_TextChanged(object sender, EventArgs e)
+        {
+            spawn3[0] = textBox13.Text;
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+            spawn3[1] = textBox12.Text;
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+            spawn3[2] = textBox11.Text;
+        }
 
     }
 }
