e43e241687d1f591761ff06e82a51ae1e68b5cdc
Samantha Chen <sc2337@cornell.edu>
2014-03-02 13:24:02 -0500

    Fixed spelling mistake

--- RTSGame/RTSEngine/Data/Team/RTSUnitInstance.cs
+++ RTSGame/RTSEngine/Data/Team/RTSUnitInstance.cs
@@ -9,14 +9,14 @@ namespace RTSEngine.Data.Team
 {
     // TODO: Implement IDestructibleEntity, ICombatEntity, IMovingEntity
 
-    class RTSUnitInstance : IDestructibleEntity, ICombatEntity, IMovingEntity
+    class RTSUnitInstance
     {
 
         //RTSUnit Data of The Unit
         protected RTSUnit UnitData;
 
         //MovementController of The Unit
-        protected IMovementContoller MovementController;
+        protected IMovementController MovementController;
 
         //ActionController of The Unit
         protected IActionController ActionController;
@@ -27,49 +27,53 @@ namespace RTSEngine.Data.Team
         //CombatController of The Unit
         protected ICombatController CombatController;
 
-            
+
         protected RTSTeam RTSTeam;
         protected Vector3 Position;
         protected ICollidable Shape;
         protected IEntity CurrentTarget;
-        protected Boolean AbleToAttack;   
+        protected Boolean AbleToAttack;
 
         // The Entity's Team
-        public RTSTeam Team { 
+        public RTSTeam Team
+        {
             get { return RTSTeam; }
         }
 
         // Location In The World
-        public Vector3 WorldPosition { 
-            get { return Position; } 
+        public Vector3 WorldPosition
+        {
+            get { return Position; }
         }
 
         // Collision Geometry
-        public ICollidable CollisionGeometry {
+        public ICollidable CollisionGeometry
+        {
             get { return Shape; }
         }
 
         // Targetting Information 
-        public IEntity Target { 
-            get { return CurrentTarget;}
+        public IEntity Target
+        {
+            get { return CurrentTarget; }
         }
 
         // Speed Of Movement For The Entity
-        public float MovementSpeed {
+        public float MovementSpeed
+        {
             get { return UnitData.MovementSpeed; }
         }
 
         // The Current Health Of The Entity
-        public int Health {
-            get { return UnitData.Health ; }
+        public int Health
+        {
+            get { return UnitData.Health; }
         }
 
         // Information About Whether This Entity Can Attack Yet
-        public bool CanAttack { 
+        public bool CanAttack
+        {
             get { return AbleToAttack; }
         }
-
-
-
     }
 }
