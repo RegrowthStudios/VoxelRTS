041b51cd7612241841c1c98649f1a4b31d11364a
Samantha Chen <sc2337@cornell.edu>
2014-03-02 14:14:09 -0500

    Added methods to RTSUnitInstance

--- RTSGame/RTSEngine/Data/Team/RTSUnitInstance.cs
+++ RTSGame/RTSEngine/Data/Team/RTSUnitInstance.cs
@@ -9,24 +9,23 @@ namespace RTSEngine.Data.Team
 {
     // TODO: Implement IDestructibleEntity, ICombatEntity, IMovingEntity
 
-    class RTSUnitInstance
+    class RTSUnitInstance : IDestructibleEntity, ICombatEntity, IMovingEntity
     {
 
-        //RTSUnit Data of The Unit
+        // RTSUnit Data of The Unit
         protected RTSUnit UnitData;
 
-        //MovementController of The Unit
+        // MovementController of The Unit
         protected IMovementController MovementController;
 
-        //ActionController of The Unit
+        // ActionController of The Unit
         protected IActionController ActionController;
 
-        //TargettingController of The Unit
+        // TargettingController of The Unit
         protected ITargettingController TargettingController;
 
-        //CombatController of The Unit
+        // CombatController of The Unit
         protected ICombatController CombatController;
-
 
         protected RTSTeam RTSTeam;
         protected Vector3 Position;
@@ -56,6 +55,12 @@ namespace RTSEngine.Data.Team
         public IEntity Target
         {
             get { return CurrentTarget; }
+            set 
+            {
+                CurrentTarget = value;
+                if (OnNewAttackTarget != null)
+                    OnNewAttackTarget(this, (IDestructibleEntity) CurrentTarget); 
+            }
         }
 
         // Speed Of Movement For The Entity
@@ -75,5 +80,34 @@ namespace RTSEngine.Data.Team
         {
             get { return AbleToAttack; }
         }
+
+        public RTSUnitInstance(RTSTeam team, Vector3 position, ICollidable shape)
+        {
+            this.RTSTeam = team;
+            this.Position = position;
+            this.Shape = shape;
+        }
+
+        // Computes The Damage To Deal With Access To A Random Number
+        public int DealDamage(double rand)
+        {
+            return UnitData.BaseCombatData.ComputeDamageDealt(rand);
+        }
+
+        // Applies Damage To Health
+        public void Damage(int d)
+        {
+            if (OnDamage != null)
+                OnDamage(this, d);
+
+            UnitData.Health -= d;
+        }
+
+        // Event Triggered When This Entity Receives Damage
+        public event Action<IDestructibleEntity, int> OnDamage;
+
+        // Event Triggered When This Entity Find A New Attack Target (Null When Can't Find One)
+        public event Action<ICombatEntity, IDestructibleEntity> OnNewAttackTarget;
+
     }
 }
