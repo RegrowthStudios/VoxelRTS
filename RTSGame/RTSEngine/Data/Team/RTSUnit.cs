0fd129674c28108c9baa324e61b679267e49ec12
Samantha Chen <sc2337@cornell.edu>
2014-03-02 13:14:38 -0500

    Added fields and properties to RTSUnitInstance

--- RTSGame/RTSEngine/Data/Team/RTSUnit.cs
+++ RTSGame/RTSEngine/Data/Team/RTSUnit.cs
@@ -5,7 +5,7 @@ using System.Text;
 using RTSEngine.Interfaces;
 
 namespace RTSEngine.Data.Team {
-    // TODO: Implement IDestructibleEntity, ICombatEntity, IMovingEntity
+    
     public class RTSUnit {
         // Health Of The Unit
         public int Health;
--- RTSGame/RTSEngine/Data/Team/RTSUnitInstance.cs
+++ RTSGame/RTSEngine/Data/Team/RTSUnitInstance.cs
@@ -2,21 +2,74 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using RTSEngine.Interfaces;
 
 namespace RTSEngine.Data.Team
 {
-    class RTSUnitInstance
+    // TODO: Implement IDestructibleEntity, ICombatEntity, IMovingEntity
+
+    class RTSUnitInstance : IDestructibleEntity, ICombatEntity, IMovingEntity
     {
+
         //RTSUnit Data of The Unit
-        protected RTSUnit RTSUnitData;
+        protected RTSUnit UnitData;
 
-        //IMovementController of The Unit
-        protected Interfaces.IMovementContoller IMovementController;
+        //MovementController of The Unit
+        protected IMovementContoller MovementController;
 
-        //IActionController of The Unit
-        protected Interfaces.IActionController IActionController;
+        //ActionController of The Unit
+        protected IActionController ActionController;
 
-        //ITargetting Controller of The Unit
-        
+        //TargettingController of The Unit
+        protected ITargettingController TargettingController;
+
+        //CombatController of The Unit
+        protected ICombatController CombatController;
+
+            
+        protected RTSTeam RTSTeam;
+        protected Vector3 Position;
+        protected ICollidable Shape;
+        protected IEntity CurrentTarget;
+        protected Boolean AbleToAttack;   
+
+        // The Entity's Team
+        public RTSTeam Team { 
+            get { return RTSTeam; }
+        }
+
+        // Location In The World
+        public Vector3 WorldPosition { 
+            get { return Position; } 
+        }
+
+        // Collision Geometry
+        public ICollidable CollisionGeometry {
+            get { return Shape; }
+        }
+
+        // Targetting Information 
+        public IEntity Target { 
+            get { return CurrentTarget;}
+        }
+
+        // Speed Of Movement For The Entity
+        public float MovementSpeed {
+            get { return UnitData.MovementSpeed; }
+        }
+
+        // The Current Health Of The Entity
+        public int Health {
+            get { return UnitData.Health ; }
+        }
+
+        // Information About Whether This Entity Can Attack Yet
+        public bool CanAttack { 
+            get { return AbleToAttack; }
+        }
+
+
+
     }
 }
