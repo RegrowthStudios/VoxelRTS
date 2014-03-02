3f2211cf1ff08d14b1acee288c69c1a8ebb5e10f
Samantha Chen <sc2337@cornell.edu>
2014-03-02 18:25:03 -0500

    Completed MovementController

--- RTSCombatSim/RTSCombatSim/Controllers/MovementContoller.cs
+++ RTSCombatSim/RTSCombatSim/Controllers/MovementContoller.cs
@@ -8,16 +8,17 @@ using RTSEngine.Data;
 
 namespace RTSCS.Controllers {
     public class MovementContoller : IMovementController {
-        // TODO: store decided move
+
+        protected Vector2 move;
 
         // The Entity That This MovementController Is Moving
-        private IMovingEntity entity;
+        protected IMovingEntity entity;
         public IEntity Entity {
             get { return entity; }
         }
 
         // List Of Waypoints To Move Each Target
-        private Vector2[] waypoints;
+        protected Vector2[] waypoints;
         public IEnumerable<Vector2> Waypoints {
             get { return waypoints; }
         }
@@ -27,17 +28,25 @@ namespace RTSCS.Controllers {
             waypoints = p;
         }
 
+        // Constructs a MovementController to Move the Given Entity to the Waypoints
+        public MovementContoller(IMovingEntity entity, Vector2[] waypoints) {
+            this.entity = entity;
+            this.waypoints = waypoints;
+        }
+
         // Performs The Critical Logic Of This Controller
         public void DecideMove(GameState g, float dt) {
-            
+            move = waypoints[waypoints.Length - 1];
         }
 
         public void ApplyMove(GameState g, float dt) {
-
+            Vector3 change;
+            Vector2 position = new Vector2(entity.WorldPosition.X, entity.WorldPosition.Y);
+            Vector2 unitChange = new Vector2(move.X - position.X, move.Y - position.Y);
+            unitChange.Normalize();
+            change = new Vector3(unitChange.X * entity.MovementSpeed * dt,unitChange.Y * entity.MovementSpeed * dt,0f);
+            entity.Move(change);
         }
 
-        public MovementContoller(Vector2[] p) {
-            waypoints = p;
-        }
     }
 }
