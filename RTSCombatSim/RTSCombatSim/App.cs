a9662ad1cea397610e2f24902a1e4460e3ecd2de
czaloj <czaloj@gmail.com>
2014-03-03 23:15:34 -0500

    And Don't Show It At The Beginning

--- RTSCombatSim/RTSCombatSim/App.cs
+++ RTSCombatSim/RTSCombatSim/App.cs
@@ -322,10 +322,6 @@ namespace RTSCS {
 
         #region Entry Point
         private static void RunMainInstance(string[] args) {
-            using(var f = new ScriptControlForm(null)){
-                f.ShowDialog();
-            }
-
             using(app = new App()) {
                 form = null;
 
