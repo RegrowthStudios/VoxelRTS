36a56575ba4a8c1558403a6fb85779658916d724
tk474 <tk474@cornell.edu>
2014-03-15 01:04:49 -0400

    Frustum changes

--- RTSGame/RTSEngine/Data/SelectionDetection.cs
+++ RTSGame/RTSEngine/Data/SelectionDetection.cs
@@ -7,17 +7,29 @@ using Microsoft.Xna.Framework;
 namespace RTSEngine.Data {
     #region Detection Objects Generated From A Camera
     public struct Frustum {
+        // 0=near, 1=far, 2=bottom, 3=top, 4=left, 5=right
+        public Plane[] planes = new Plane[6];
 
         public Frustum(Matrix mView, Matrix mProj, Vector2 min, Vector2 max) {
             // TODO: From An PerspectiveProjection
             BoundingFrustum f = new BoundingFrustum(mView * mProj);
+            planes[0] = f.Near;
+            planes[1] = f.Far;
+            planes[2] = f.Bottom;
+            planes[3] = f.Top;
+            planes[4] = f.Left;
+            planes[5] = f.Right;
         }
     }
-    public struct OBB {
 
+    public struct OBB {
+        public Vector3 min;
+        public Vector3 max;
         public OBB(Matrix mView, Matrix mProj, Vector2 min, Vector2 max) {
             // TODO: From An Orthographic Projection
             BoundingBox b = new BoundingBox();
+            this.min = b.Min;
+            this.max = b.Max;
         }
     }
     #endregion
