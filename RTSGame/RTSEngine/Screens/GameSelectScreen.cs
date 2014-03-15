6104738ee490c6ccff0aa0bde27d1b0fc21c369f
chungbung22 <cc733@cornell.edu>
2014-03-15 16:56:03 -0400

    Fixed Conflict Error

---
+++ RTSGame/RTSEngine/Screens/GameSelectScreen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BlisterUI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using RTSEngine.Graphics;
+
+namespace RTSEngine.Screens
+{
+    public class GameSelectScreen : GameScreenIndexed
+    {
+        public GameSelectScreen(int i) : base(i) { }
+        public GameSelectScreen(int p, int n) : base(p, n) { }
+        public override void Build()
+        {
+        }
+        public override void Destroy(GameTime gameTime)
+        {
+        }
+        public override void OnEntry(GameTime gameTime)
+        {
+        }
+        public override void OnExit(GameTime gameTime)
+        {
+
+        }
+        public override void Update(GameTime gameTime)
+        {
+        }
+        public override void Draw(GameTime gameTime)
+        {
+        }
+    }
+}
