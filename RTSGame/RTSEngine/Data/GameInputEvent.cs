5dad0dfa09682574ae95036983a0315f8c39a949
czaloj <czaloj@gmail.com>
2014-03-15 20:06:35 -0400

    Appropriate Game Event Types

--- RTSGame/RTSEngine/Data/GameInputEvent.cs
+++ RTSGame/RTSEngine/Data/GameInputEvent.cs
@@ -7,37 +7,56 @@ using Microsoft.Xna.Framework;
 using RTSEngine.Interfaces;
 
 namespace RTSEngine.Data {
+    public enum GameEventType {
+        Select,
+        SetWaypoint,
+        SetTarget
+    }
+
     public class GameInputEvent {
-        public enum Action {
-            Select,
-            SetWaypoint,
-            SetTarget,
+        public GameEventType Action {
+            get;
+            private set;
         }
-        public Action action { get; protected set; }
+
+        public GameInputEvent(GameEventType a) {
+            Action = a;
+        }
     }
 
     public class SelectEvent : GameInputEvent {
-        public RTSSquad Selected { get; private set; }   
-        public SelectEvent(RTSSquad s){
+        public List<IEntity> Selected {
+            get;
+            private set;
+        }
+
+        public SelectEvent(List<IEntity> s)
+            : base(GameEventType.Select) {
             Selected = s;
-            action = Action.Select;
         }
     }
 
     public class SetWayPoint : GameInputEvent {
-        public Vector2 waypoint { get; private set; }
-        public SetWayPoint(Vector2 w) {
-            waypoint = w;
-            action = Action.SetWaypoint;
+        public Vector2 Waypoint {
+            get;
+            private set;
+        }
+
+        public SetWayPoint(Vector2 w)
+            : base(GameEventType.SetWaypoint) {
+            Waypoint = w;
         }
     }
 
     public class SetTargetEvent : GameInputEvent {
-        public IDestructibleEntity Target { get; private set; }
-        public SetTargetEvent(IDestructibleEntity t) {
+        public IDestructibleEntity Target {
+            get;
+            private set;
+        }
+
+        public SetTargetEvent(IDestructibleEntity t)
+            : base(GameEventType.SetTarget) {
             Target = t;
-            action = Action.SetTarget;
         }
     }
-
 }
