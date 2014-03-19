495ac5ec6488a2b94da55cebe61a72f35676309c
czaloj <czaloj@gmail.com>
2014-03-19 09:53:06 -0400

    Separated Rendering Passes

--- RTSGame/RTSEngine/Graphics/RTSRenderer.cs
+++ RTSGame/RTSEngine/Graphics/RTSRenderer.cs
@@ -43,13 +43,16 @@ namespace RTSEngine.Graphics {
         }
 
         // Effects
-        private BasicEffect fxMap;
+        private BasicEffect fxMap, fxSelection;
         private RTSEffect fxRTS;
 
         public RTSRenderer(GraphicsDeviceManager gdm, string rtsFXFile, GameWindow w) {
             window = w;
             gManager = gdm;
             UnitModels = new List<RTSUnitModel>();
+
+            tPixel = new Texture2D(G, 1, 1);
+            tPixel.SetData(new Color[] { Color.White });
 
             fxMap = new BasicEffect(G);
             fxMap.LightingEnabled = false;
@@ -58,14 +61,19 @@ namespace RTSEngine.Graphics {
             fxMap.TextureEnabled = true;
             fxMap.World = Matrix.Identity;
 
+            fxSelection = new BasicEffect(G);
+            fxSelection.LightingEnabled = false;
+            fxSelection.FogEnabled = false;
+            fxSelection.TextureEnabled = false;
+            fxSelection.VertexColorEnabled = true;
+            fxSelection.World = Matrix.Identity;
+            fxSelection.Texture = tPixel;
+
             fxRTS = new RTSEffect(XNAEffect.Compile(G, rtsFXFile));
             fxRTS.World = Matrix.Identity;
             fxRTS.CPrimary = Vector3.UnitX;
             fxRTS.CSecondary = Vector3.UnitY;
             fxRTS.CTertiary = Vector3.UnitZ;
-
-            tPixel = new Texture2D(G, 1, 1);
-            tPixel.SetData(new Color[] { Color.White });
 
             drawBox = false;
             MouseEventDispatcher.OnMousePress += OnMousePress;
@@ -86,6 +94,75 @@ namespace RTSEngine.Graphics {
             Map.Dispose();
 
             fxMap.Dispose();
+            fxSelection.Dispose();
+        }
+
+        // Rendering Passes
+        public void Draw(GameState s, float dt) {
+            G.Clear(Color.Black);
+
+            // TODO: Draw Environment Cube
+
+            DrawMap();
+            DrawAnimated();
+            if(drawBox) DrawSelectionBox();
+        }
+        private void DrawMap() {
+            // Set States
+            G.DepthStencilState = DepthStencilState.Default;
+            G.RasterizerState = RasterizerState.CullCounterClockwise;
+            G.BlendState = BlendState.Opaque;
+            G.SamplerStates[0] = SamplerState.LinearClamp;
+
+            // Set Camera
+            fxMap.View = camera.View;
+            fxMap.Projection = camera.Projection;
+
+            // Primary Map Model
+            Map.SetPrimaryModel(G);
+            fxMap.Texture = Map.PrimaryTexture;
+            fxMap.CurrentTechnique.Passes[0].Apply();
+            Map.DrawPrimary(G);
+
+            // Secondary Map Model
+            Map.SetSecondaryModel(G);
+            fxMap.Texture = Map.SecondaryTexture;
+            fxMap.CurrentTechnique.Passes[0].Apply();
+            Map.DrawSecondary(G);
+        }
+        private void DrawAnimated() {
+            // Set Camera
+            fxRTS.VP = camera.View * camera.Projection;
+
+            // Loop Through Models
+            foreach(RTSUnitModel unitModel in UnitModels) {
+                fxRTS.TexModelMap = unitModel.AnimationTexture;
+                fxRTS.TexOverlay = unitModel.ModelTexture;
+                fxRTS.TexColor = unitModel.ColorCodeTexture;
+                fxRTS.ApplyPassAnimation();
+                unitModel.UpdateInstances(G);
+                unitModel.SetInstances(G);
+                unitModel.DrawInstances(G);
+            }
+        }
+        private void DrawSelectionBox() {
+            Vector2 ss = new Vector2(G.Viewport.TitleSafeArea.Width, G.Viewport.TitleSafeArea.Height);
+            fxSelection.View = Matrix.CreateLookAt(new Vector3(ss / 2, -1), new Vector3(ss / 2, 0), Vector3.Down);
+            fxSelection.Projection = Matrix.CreateOrthographic(ss.X, ss.Y, 0, 2);
+
+            G.DepthStencilState = DepthStencilState.None;
+            G.BlendState = BlendState.NonPremultiplied;
+            G.RasterizerState = RasterizerState.CullNone;
+
+            Vector3 min = new Vector3(Vector2.Min(start, end), 0);
+            Vector3 max = new Vector3(Vector2.Max(start, end), 0);
+            fxSelection.CurrentTechnique.Passes[0].Apply();
+            G.DrawUserPrimitives(PrimitiveType.TriangleStrip, new VertexPositionColor[] {
+                    new VertexPositionColor(min, new Color(0f, 0, 1f, 0.3f)),
+                    new VertexPositionColor(new Vector3(max.X, min.Y, 0), new Color(1f, 0, 1f, 0.3f)),
+                    new VertexPositionColor(new Vector3(min.X, max.Y, 0), new Color(1f, 0, 1f, 0.3f)),
+                    new VertexPositionColor(max, new Color(1f, 0, 0f, 0.3f)),
+                }, 0, 2, VertexPositionColor.VertexDeclaration);
         }
 
         // Selection Box Handling
@@ -102,71 +179,6 @@ namespace RTSEngine.Graphics {
         }
         private void OnMouseMove(Vector2 p, Vector2 d) {
             end = p;
-        }
-
-        public void Draw(GameState s, float dt) {
-            G.Clear(Color.Black);
-
-            // TODO: Draw Environment Cube
-
-            // Setup Camera Matrices
-            fxMap.View = camera.View;
-            fxMap.Projection = camera.Projection;
-            fxRTS.VP = camera.View * camera.Projection;
-
-            // Draw The Map
-            G.DepthStencilState = DepthStencilState.Default;
-            G.RasterizerState = RasterizerState.CullCounterClockwise;
-            G.BlendState = BlendState.Opaque; Map.SetPrimaryModel(G);
-            // Primary Map Model
-            fxMap.Texture = Map.PrimaryTexture;
-            G.SamplerStates[0] = SamplerState.LinearClamp;
-            fxMap.CurrentTechnique.Passes[0].Apply();
-            Map.DrawPrimary(G);
-            // Secondary Map Model
-            fxMap.Texture = Map.SecondaryTexture;
-            G.SamplerStates[0] = SamplerState.LinearClamp;
-            Map.SetSecondaryModel(G);
-            fxMap.CurrentTechnique.Passes[0].Apply();
-            Map.DrawSecondary(G);
-
-
-            // Draw All Animated Entities
-            foreach(RTSUnitModel unitModel in UnitModels) {
-                fxRTS.TexModelMap = unitModel.AnimationTexture;
-                fxRTS.TexOverlay = unitModel.ModelTexture;
-                fxRTS.TexColor = unitModel.ColorCodeTexture;
-                fxRTS.ApplyPassAnimation();
-                unitModel.UpdateInstances(G);
-                unitModel.SetInstances(G);
-                unitModel.DrawInstances(G);
-            }
-
-            if(drawBox) {
-                fxMap.VertexColorEnabled = true;
-                fxMap.TextureEnabled = false;
-                fxMap.Texture = tPixel;
-                Vector2 ss = new Vector2(G.Viewport.Width, G.Viewport.Height);
-                fxMap.View = Matrix.CreateLookAt(new Vector3(ss / 2, -1), new Vector3(ss / 2, 0), Vector3.Down);
-                fxMap.Projection = Matrix.CreateOrthographic(ss.X, ss.Y, 0, 2);
-                G.DepthStencilState = DepthStencilState.None;
-                G.BlendState = BlendState.NonPremultiplied;
-                G.RasterizerState = RasterizerState.CullNone;
-                Vector3 min = new Vector3(Vector2.Min(start, end), 0);
-                Vector3 max = new Vector3(Vector2.Max(start, end), 0);
-                fxMap.CurrentTechnique.Passes[0].Apply();
-                G.DrawUserPrimitives(PrimitiveType.TriangleStrip, new VertexPositionColor[] {
-                    new VertexPositionColor(min, new Color(0f, 0, 1f, 0.3f)),
-                    new VertexPositionColor(new Vector3(max.X, min.Y, 0), new Color(1f, 0, 1f, 0.3f)),
-                    new VertexPositionColor(new Vector3(min.X, max.Y, 0), new Color(1f, 0, 1f, 0.3f)),
-                    new VertexPositionColor(max, new Color(1f, 0, 0f, 0.3f)),
-                }, 0, 2, VertexPositionColor.VertexDeclaration);
-
-                fxMap.View = camera.View;
-                fxMap.Projection = camera.Projection;
-                fxMap.VertexColorEnabled = false;
-                fxMap.TextureEnabled = true;
-            }
         }
     }
 }
