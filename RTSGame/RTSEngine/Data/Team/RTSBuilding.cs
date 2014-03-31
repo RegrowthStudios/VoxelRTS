5085c7a4d88468503cc33a33d044f4c508e09e1c
Tatsuhiro Koshi <tk474@cornell.edu>
2014-03-31 13:25:32 -0400

    RTS Building

---
+++ RTSGame/RTSEngine/Data/Team/RTSBuilding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using RTSEngine.Interfaces;
+
+namespace RTSEngine.Data.Team
+{
+    public class RTSBuilding : IEntity
+    {
+        public int Health;
+        public int CapitalCost { get; private set; }
+        public RTSTeam Team { get; private set; }
+        public Vector2 GridPosition;
+        public float Height;
+        public ICollidable ICollidableShape;
+        public BoundingBox BBox;
+
+        public Vector3 WorldPosition {
+            get { return new Vector3(GridPosition.X, Height, GridPosition.Y); }
+        }
+
+        public RTSBuilding() {
+            
+        }
+
+    }
+}
