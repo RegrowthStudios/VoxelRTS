817908f3672bafa3ecaa58d1b4604de0994d9e00
Cristian Zaloj <czaloj@gmail.com>
2014-05-15 20:26:23 -0400

    Fixed Menu Screen

---
+++ RTSGame/Defiant/Defiant/OptionsScreen.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using BlisterUI;
+using BlisterUI.Input;
+using BlisterUI.Widgets;
+
+namespace RTS {
+    public class OptionsScreen : GameScreen<App> {
+        public override int Next {
+            get { return -1; }
+            protected set { }
+        }
+        public override int Previous {
+            get { return game.MenuScreen.Index; }
+            protected set { }
+        }
+
+        public override void Build() {
+        }
+        public override void Destroy(GameTime gameTime) {
+        }
+
+        public override void OnEntry(GameTime gameTime) {
+            KeyboardEventDispatcher.OnKeyPressed += OnKP;
+        }
+        public override void OnExit(GameTime gameTime) {
+            KeyboardEventDispatcher.OnKeyPressed -= OnKP;
+        }
+
+        public override void Update(GameTime gameTime) {
+        }
+        public override void Draw(GameTime gameTime) {
+            G.Clear(Color.Black);
+
+            game.DrawMouse();
+        }
+
+        private void OnKP(object sender, KeyEventArgs args) {
+            switch(args.KeyCode) {
+                case Keys.P:
+                case Keys.Escape:
+                    State = ScreenState.ChangePrevious;
+                    break;
+            }
+        }
+    }
+}
